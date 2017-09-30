5c11bea5e13d72a3ea0caf7c9c321eff1f288bce
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-09-30 20:55:11 +0200

    message is contenteditable. message is sent as JSON POST.

--- public/chat_client.js
+++ public/chat_client.js
@@ -1,60 +1,56 @@
 var base_dir = window.location.pathname.split('/').slice(0,-1).join('/');
 
-// jQuery plugin to prevent double submission of forms
-jQuery.fn.preventDoubleSubmission = function() {
-  $(this).on('submit',function(e){
-    var $form = $(this);
-
-    if ($form.data('submitted') === true) {
-      // Previously submitted - don't submit again
-      e.preventDefault();
-    } else {
-      // Mark it so that the next submit can be ignored
-      $form.data('submitted', true);
-    }
-  });
-
-  // Keep chainability
-  return this;
-};
-
 // chat scrolling
-function scrollHeight(){ return $(document).height()-$(window).height() }
-function isScrolledToBottom(){ return $('html')[0].scrollTop == scrollHeight() }
-function scrollToBottom(){ $('html')[0].scrollTop = scrollHeight() }
+function scrollHeight(){ return $(document).height()-$(window).height(); }
+function isScrolledToBottom(){ return $('html')[0].scrollTop == scrollHeight(); }
+function scrollToBottom(){ $('html')[0].scrollTop = scrollHeight(); }
 
 // message listing
-function listMessages(scrollFlag){ $('#message_log').load(base_dir+'/chat/list', function(){ if(scrollFlag) scrollToBottom() } ) }
-function periodicallyListMessagesCallback(){ var scrollFlag = isScrolledToBottom(); listMessages(scrollFlag) }
+function listMessages(scrollFlag){ $('#message_log').load(base_dir+'/chat/list', function(){ if(scrollFlag) scrollToBottom(); } ) }
+function periodicallyListMessagesCallback(){ var scrollFlag = isScrolledToBottom(); listMessages(scrollFlag); }
+
+// allow message sending
+function send_message(){
+
+    var form_data_object = {
+        sender: $('input#sender').val().trim(),
+        text: $('div#message_text').html().trim(),
+    };
+
+    // no blank message fields allowed
+    if(form_data_object.text=='' || form_data_object.text==''){
+         alert('no blank message fields allowed!');
+         return false;
+    }
+
+    // disable form on pre-submit
+    $('#send_message_form *').prop('disabled', true);
+
+    // send JSON with POST type HTTP request
+    $.ajax({
+        type: 'POST',
+        url: base_dir+'/chat/send',
+        dataType: 'text',
+        data: JSON.stringify(form_data_object),
+    }).done(function(){
+        // empty the message field
+        $('div#message_text').html('');
+    })
+    .fail(function(){ // error handling
+        alert('Error! (when sending a message)');
+    })
+    .always(function(){
+        // enable form on post-submit
+        $('#send_message_form *').prop('disabled', false);
+    });
+}
 
 // on ready
 $(function(){
 
-	// stay updated
-	setInterval(periodicallyListMessagesCallback, 1000) // get messages periodically
+    // stay updated
+    setInterval(periodicallyListMessagesCallback, 1000); // get messages periodically
 
-	// allow message sending
-	$('form#send_message_form').preventDoubleSubmission();
-	$('form#send_message_form').submit(function(e){
-		e.preventDefault() // prevent page reload
+    $('#send').click(send_message);
+});
 
-		var input = $('input[name=message_text]');
-		if(input.val()=='') return false // no blank message allowed
-		var serialized_form = $('form#send_message_form').serialize();
-		input.prop('disabled', true) // disable input on submit
-
-		// send
-		$.post(base_dir+'/chat/send', serialized_form) // send message
-		.done(function(){
-			input.val('')
-		})
-		.fail(function(){ // error handling
-			alert('Error! (when sending a message)')
-		})
-		.always(function(){
-			input.prop('disabled', false) // restore input after submit
-			input.focus()
-		})
-		return false
-	})
-})
--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@ require 'pdo.php';
 
 // phpinfo()
 $app->get('/util/phpinfo', function ($request, $response, $args) {
-	phpinfo();
+    phpinfo();
 });
 
 // setup database tables
@@ -24,27 +24,28 @@ $app->get('/', function ($request, $response, $args) {
 
 // list messages
 $app->get('/chat/list', function ($request, $response, $args) use ($app) {
-	// OUT: $messages
-	$messages = pdo_execute('SELECT * FROM (SELECT * FROM chat_messages ORDER BY creation_timestamp DESC LIMIT 15) AS res ORDER BY creation_timestamp ASC');
-	// - produce HTML output
-	// IN: $messages OUT: $output
-	$output = '';
-	foreach($messages as $message) {
-		$sender = htmlspecialchars($message['sender']);
-		$text = htmlspecialchars($message['message_text']);
-		$output .= "<div class=\"line\">$sender: $text</div>";
-	}
-	echo $output;
+    // OUT: $messages
+    $messages = pdo_execute('SELECT * FROM (SELECT * FROM chat_messages ORDER BY creation_timestamp DESC LIMIT 15) AS res ORDER BY creation_timestamp ASC');
+    // - produce HTML output
+    // IN: $messages OUT: $output
+    $output = '';
+    foreach($messages as $message) {
+        $sender = htmlspecialchars($message['sender']);
+        $text = htmlspecialchars($message['message_text']);
+        $output .= "<div class=\"line\">$sender: $text</div>";
+    }
+    echo $output;
 });
 
 // insert new message
 $app->post('/chat/send', function ($request, $response, $args) {
+    $unparsedBodyJSON = $request->getBody();
     // IN: $request OUT: $message
-    $postVars = $request->getParsedBody();
-    $text = $postVars["message_text"];
-    $sender = $postVars["sender"];
-    $message = ['text' => $text, 'sender' => $sender];    
+    //$unparsedBodyJSON = $request->getBody();
+    // parse the JSON into an associative array
+    $message = json_decode($unparsedBodyJSON, true);
+    // $message has value ['text' => ..., 'sender' => ...];    
     // - SQL chat send
-	// IN: $message
-	pdo_execute('INSERT INTO chat_messages (message_text, sender) VALUES (:text, :sender)', $message);
+    // IN: $message
+    pdo_execute('INSERT INTO chat_messages (message_text, sender) VALUES (:text, :sender)', $message);
 });
