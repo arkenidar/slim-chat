0ceb081eb593ee65ec95e0a6b010748859c7c109
Dario Cangialosi, Coder <arkenidar@users.noreply.github.com>
2017-10-03 06:37:28 +0200

    defaults to sqlite

--- src/pdo_conf.php
+++ src/pdo_conf.php
@@ -1,7 +1,7 @@
 <?php
 
 // supported DB types: 'sqlite', 'postgres', 'mysql'
-define('pdo_db_type', 'mysql');
+define('pdo_db_type', 'sqlite');
 
 // constants for storing passwords
 define('postgres_password', 'password');
