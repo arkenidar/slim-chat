9af4c2aa2dac320d9e8c3f30101da103b5c70d2f
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-10-18 17:07:25 +0200

    windows startup script aligned to bash startup script.

--- src/util/pdo_conf.php
+++ src/util/pdo_conf.php
@@ -1,7 +1,7 @@
 <?php
 
 // supported DB types: 'sqlite', 'postgres', 'mysql'
-define('pdo_db_type', 'sqlite');
+define('pdo_db_type', 'mysql');
 
 // constants for storing passwords
 define('postgres_password', 'password');
